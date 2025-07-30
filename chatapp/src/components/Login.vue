407c1b9b55b165c745d93c0a081e741e4d0fe860
ChiguChigu-Tech <21rd128.seki@gmail.com>
2025-07-30 15:57:06 +0900

    Chat画面へのユーザー名の送信機能の追加

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -14,20 +14,23 @@ const socket = socketManager.getInstance()
 
 // #region reactive variable
 const inputUserName = ref("")
-// #endregion
+
 
 // #region browser event handler
 // 入室メッセージをクライアントに送信する
 const onEnter = () => {
   // ユーザー名が入力されているかチェック
-
-  // 入室メッセージを送信
-
-  // 全体で使用するnameに入力されたユーザー名を格納
   if (inputUserName.value.trim() === "") {
     alert("ユーザー名を入力してください")
     return
   }
+
+  // 入室メッセージを送信
+
+
+  // 全体で使用するnameに入力されたユーザー名を格納
+  userName.value = inputUserName.value.trim()
+
   // チャット画面へ遷移
   router.push({ name: "chat" })
 }
@@ -39,7 +42,7 @@ const onEnter = () => {
     <h1 class="text-h3 font-weight-medium">Vue.js Chat サンプル</h1>
     <div class="mt-10">
       <p>ユーザー名</p>
-      <input type="text" class="user-name-text" />
+      <input type="text" v-model="inputUserName" class="user-name-text" /> 
     </div>
     <button type="button" @click="onEnter" class="button-normal">入室する</button>
   </div>
